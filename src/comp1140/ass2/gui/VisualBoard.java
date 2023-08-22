0b0b010e3323ef342160aa68cffbbee66d6550cd
Koen Dubrow <u7646615@anu.edu.au>
2023-08-22 22:52:00 +1000

    Completed VisualBoard class skeleton

---
+++ src/comp1140/ass2/gui/VisualBoard.java
@@ -0,0 +1,16 @@
+package comp1140.ass2.gui;
+
+public class VisualBoard {
+
+    private Board board;
+
+    /*Backend board that this visual board corresponds to*/
+    private VisualBoard(Board board) {
+
+    }
+
+    /*Places piece on board*/
+    private void piecePlacement(MoveablePiece piece) {
+
+    }
+}
