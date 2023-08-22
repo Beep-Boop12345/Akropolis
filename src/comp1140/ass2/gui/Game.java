6d6d35eb7b34035d3a9c875b44a1c18ef22ac42d
Koen Dubrow <u7646615@anu.edu.au>
2023-08-22 22:43:41 +1000

    Added field variable to Game skeleton

--- src/comp1140/ass2/gui/Game.java
+++ src/comp1140/ass2/gui/Game.java
@@ -1,5 +1,6 @@
 package comp1140.ass2.gui;
 
+import comp1140.ass2.Akropolis;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -14,6 +15,14 @@ public class Game extends Application {
     private final Group root = new Group();
     private static final int WINDOW_WIDTH = 1200;
     private static final int WINDOW_HEIGHT = 700;
+
+    private static Akropolis akropolis;
+
+    private static VisualBoard[] boards = new VisualBoard[4];
+
+    private static VisualConstructionSite purchasablePieces;
+
+    private static PieceStack pieceStack;
 
     @Override
     public void start(Stage stage) throws Exception {
