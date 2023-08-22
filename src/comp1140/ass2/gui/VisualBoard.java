777ad72791d191dc3966f6a9112eb46481f1c2e5
Koen Dubrow <u7646615@anu.edu.au>
2023-08-22 22:57:06 +1000

    Completed VisualConstructionSite class fields for skeleton

--- src/comp1140/ass2/gui/VisualBoard.java
+++ src/comp1140/ass2/gui/VisualBoard.java
@@ -13,4 +13,5 @@ public class VisualBoard {
     private void piecePlacement(MoveablePiece piece) {
 
     }
+
 }
---
+++ src/comp1140/ass2/gui/VisualConstructionSite.java
@@ -0,0 +1,9 @@
+package comp1140.ass2.gui;
+
+public class VisualConstructionSite {
+    /*Holds the pieces that can be purchased*/
+    public static PurchasablePiece[] purchasablePieces = new PurchasablePiece[11];
+
+    /*Holds the backend ConstructionSite*/
+    public static ConstructionSite site;
+}
