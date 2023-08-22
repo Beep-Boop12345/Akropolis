ce09e706c08f185717cfceec9eebf278c999a6c8
Koen Dubrow <u7646615@anu.edu.au>
2023-08-22 23:02:13 +1000

    Completed VisualPiece class methods for skeleton

--- src/comp1140/ass2/gui/PurchasablePiece.java
+++ src/comp1140/ass2/gui/PurchasablePiece.java
@@ -3,4 +3,13 @@ package comp1140.ass2.gui;
 public class PurchasablePiece {
     /*The back end piece this represents*/
     private Piece piece;
+
+    private PurchasablePiece(Piece piece) {
+
+    }
+
+    /*How the constructionSite representation responds to a click*/
+    private void onClick() {
+
+    }
 }
