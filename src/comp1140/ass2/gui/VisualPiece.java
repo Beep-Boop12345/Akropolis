96a56d96eee7613e61b0dbc5c0fe7b4c86305840
u7683699 <u7683699@anu.edu.au>
2023-09-16 15:58:42 +1000

    Upstream Pull

--- src/comp1140/ass2/gui/VisualPiece.java
+++ src/comp1140/ass2/gui/VisualPiece.java
@@ -1,5 +1,11 @@
 package comp1140.ass2.gui;
 
-public class VisualPiece {
+import javafx.scene.Group;
 
+import java.util.ArrayList;
+
+public class VisualPiece extends Group {
+    VisualPiece(ArrayList<VisualTile> tiles) {
+
+    }
 }
