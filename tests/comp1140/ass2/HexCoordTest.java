49d8d2e6bcadaa8cafd35a7c7db80bff650d9325
u7683699 <u7683699@anu.edu.au>
2023-09-27 12:51:47 +1000

    Completed get surroundings tests in hexcoord test

--- tests/comp1140/ass2/HexCoordTest.java
+++ tests/comp1140/ass2/HexCoordTest.java
@@ -20,5 +20,51 @@ class HexCoordTest {
 
     @Test
     void getSurroundings() {
+        var testHex1 = new HexCoord(0,0);
+        var testList = testHex1.getSurroundings();
+        var actualList = new HexCoord[]{
+                new HexCoord(0,1),
+                new HexCoord(1,0),
+                new HexCoord(1,-1),
+                new HexCoord(0,-1),
+                new HexCoord(-1,-1),
+                new HexCoord(-1,0)
+        };
+
+        for (int i = 0; i < testList.length; i++) {
+            assertEquals(testList[i], actualList[i]);
+        }
+
+        var testHex2 = new HexCoord(3,4);
+        var testList2 = testHex2.getSurroundings();
+        var actualList2 = new HexCoord[]{
+                new HexCoord(3,5),
+                new HexCoord(4,5),
+                new HexCoord(4,4),
+                new HexCoord(3,3),
+                new HexCoord(2,4),
+                new HexCoord(2,5)
+        };
+
+        for (int i = 0; i < testList2.length; i++) {
+            assertEquals(testList2[i], actualList2[i]);
+        }
+
+        var testHex3 = new HexCoord(-2,-5);
+        var testList3 = testHex3.getSurroundings();
+        var actualList3 = new HexCoord[]{
+                new HexCoord(-2,-4),
+                new HexCoord(-1,-5),
+                new HexCoord(-1,-6),
+                new HexCoord(-2,-6),
+                new HexCoord(-3,-6),
+                new HexCoord(-3,-5)
+        };
+
+        for (int i = 0; i < testList3.length; i++) {
+            assertEquals(testList3[i], actualList3[i]);
+        }
+
+
     }
 }
