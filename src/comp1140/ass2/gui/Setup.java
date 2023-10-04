178e08834dbb5c9cc29bf1a178a81c88e4c03727
Andrew Nguyen <u7330006@anu.edu.au>
2023-10-04 17:16:00 +1100

    Finalised setup

--- src/comp1140/ass2/gui/Setup.java
+++ src/comp1140/ass2/gui/Setup.java
@@ -1,7 +1,6 @@
 package comp1140.ass2.gui;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,25 +11,25 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
+import java.util.HashSet;
 import java.util.Random;
-
 
 
 public class Setup {
     private static String gameState;
     private static Stage primaryStage = new Stage();
-    private static final int SETUP_WIDTH = 1200;
-    private static final int SETUP_HEIGHT = 750;
+    private static final int SETUP_WIDTH = 1200*3/4;
+    private static final int SETUP_HEIGHT = 750*3/4;
     private static int playerCount = 2;
 
     private static char[] scoreVariants = {'h', 'm', 'b', 't', 'g'};
-    private static final StackPane view = new StackPane();
+    private static final StackPane background = new StackPane();
 
     private static final Group controls = new Group();
 
-    private static final Group root = new Group();
+    private static final StackPane root = new StackPane();
 
-    static Label playerCountDisplay = new Label("2");
+    private static Label playerCountDisplay = new Label("2");
 
     static Button incrementPlayersButton;
     static Button decrementPlayersButton;
@@ -47,67 +46,64 @@ public class Setup {
         playerCountLabel.setTextFill(Color.WHITE);
         playerCountLabel.setStyle("-fx-font-weight: bold;");
 
-        // Buttons to adjust the number of players
-        incrementPlayersButton = new Button("+");
-        incrementPlayersButton.setStyle("-fx-background-color: red; -fx-text-fill: black;");
-        incrementPlayersButton.setPrefWidth(25);
-        incrementPlayersButton.setPrefHeight(25);
-
-        decrementPlayersButton = new Button("-");
-        decrementPlayersButton.setStyle("-fx-background-color: yellow; -fx-text-fill: black;");
-        decrementPlayersButton.setPrefWidth(25);
-        decrementPlayersButton.setPrefHeight(25);
 
         // Create a label for displaying the player count
-        Label playerCountDisplay = new Label(Integer.toString(playerCount));
         playerCountDisplay.setStyle("-fx-font-weight: bold;");
 
-        // Checkbox buttons for scoring variants
-        housesVariant = new CheckBox("Houses Scoring Variant");
-        marketsVariant = new CheckBox("Markets Scoring Variant");
-        barracksVariant = new CheckBox("Barracks Scoring Variant");
-        templesVariant = new CheckBox("Temples Scoring Variant");
-        gardensVariant = new CheckBox("Gardens Scoring Variant");
+        // Buttons to adjust the number of players
+        incrementPlayersButton = createButton("+", "-fx-background-color: red; -fx-text-fill: black;");
+        decrementPlayersButton = createButton("-", "-fx-background-color: yellow; -fx-text-fill: black;");
 
-        // Apply bold style to checkbox labels
-        housesVariant.setStyle("-fx-font-weight: bold;");
-        marketsVariant.setStyle("-fx-font-weight: bold;");
-        barracksVariant.setStyle("-fx-font-weight: bold;");
-        templesVariant.setStyle("-fx-font-weight: bold;");
-        gardensVariant.setStyle("-fx-font-weight: bold;");
+        // Checkbox buttons for scoring variants
+        housesVariant = createCheckBox("Houses Scoring Variant");
+        marketsVariant = createCheckBox("Markets Scoring Variant");
+        barracksVariant = createCheckBox("Barracks Scoring Variant");
+        templesVariant = createCheckBox("Temples Scoring Variant");
+        gardensVariant = createCheckBox("Gardens Scoring Variant");
 
         // Create an HBox for the increment and decrement buttons, along with the player count
         HBox playerCountHBox = new HBox(50);
         playerCountHBox.setAlignment(Pos.CENTER);
         playerCountHBox.getChildren().addAll(decrementPlayersButton, playerCountDisplay, incrementPlayersButton);
-        playerCountHBox.setLayoutX(300);
-        playerCountHBox.setLayoutY(100);
 
-        // Create a VBox for the checkboxes and adjust its position
-        VBox checkboxVBox = new VBox(10);
-        checkboxVBox.setAlignment(Pos.CENTER);
-        checkboxVBox.getChildren().addAll(housesVariant, marketsVariant, barracksVariant, templesVariant, gardensVariant);
-        checkboxVBox.setLayoutX(500);
-        checkboxVBox.setLayoutY(200);
-
-        // Create a HBox for the playButton
-        playButton = new Button("Play");
-        playButton.setStyle("-fx-background-color: yellow; -fx-text-fill: black;");
-        playButton.setPrefWidth(80);
-        playButton.setPrefHeight(30);
-
-        // Create an HBox for the "Play" button and center it
+        // Create an HBox for the "Play" button
+        playButton = createButton("Play", "-fx-background-color: yellow; -fx-text-fill: black; -fx-font-weight: bold;");
+        playButton.setMinWidth(80);
         HBox playButtonHBox = new HBox(playButton);
-        playButton.setTranslateX(500);
-        playButton.setTranslateY(750/2);
+        playButtonHBox.setAlignment(Pos.CENTER);
 
-        // Create a VBox to organize the controls and set its background color to grey
-        VBox greyBox = new VBox(0);
-        greyBox.setAlignment(Pos.CENTER);
-        greyBox.setStyle("-fx-background-color: grey;");
-        greyBox.getChildren().addAll(playerCountLabel, playButtonHBox, checkboxVBox);
+        // Create a VBox to organize the controls vertically
+        VBox controlVBox = new VBox(20);
+        controlVBox.getChildren().addAll(playerCountLabel, playerCountHBox, housesVariant, marketsVariant, barracksVariant, templesVariant, gardensVariant, playButtonHBox);
+        controlVBox.setStyle("-fx-background-color: " + Color.LIGHTBLUE.interpolate(Color.TRANSPARENT, 0.5).toString().replace("0x", "#") + ";");
+        controlVBox.setAlignment(Pos.CENTER);
 
-        // Event handling for buttons
+        // Event Handling
+        setupEventHandlers();
+
+
+        // Set the root to the StackPane
+        controls.getChildren().add(controlVBox);
+    }
+
+    // Helper method to create buttons (of all the same size)
+    private static Button createButton(String text, String style) {
+        Button button = new Button(text);
+        button.setStyle(style);
+        button.setPrefWidth(25);
+        button.setPrefHeight(25);
+        return button;
+    }
+
+    // Helper method to create checkboxes (of the same size)
+    private static CheckBox createCheckBox(String text) {
+        CheckBox checkBox = new CheckBox(text);
+        checkBox.setStyle("-fx-font-weight: bold;");
+        return checkBox;
+    }
+
+    // Method for event handling
+    private static void setupEventHandlers() {
         incrementPlayersButton.setOnAction(event -> {
             if (playerCount < 4) {
                 playerCount++;
@@ -120,11 +116,6 @@ public class Setup {
                 playerCount--;
                 playerCountDisplay.setText(Integer.toString(playerCount));
             }
-        });
-
-        playButton.setOnAction(event -> {
-            String gameState = Integer.toString(playerCount) + scoreVariants;
-            System.out.println(gameState);
         });
 
         housesVariant.setOnAction(event -> {
@@ -145,85 +136,74 @@ public class Setup {
 
         barracksVariant.setOnAction(event -> {
             if (barracksVariant.isSelected()) {
-                scoreVariants[1] = 'B';
+                scoreVariants[2] = 'B';
             } else {
-                scoreVariants[1] = 'b';
+                scoreVariants[2] = 'b';
             }
         });
 
         templesVariant.setOnAction(event -> {
             if (templesVariant.isSelected()) {
-                scoreVariants[1] = 'T';
+                scoreVariants[3] = 'T';
             } else {
-                scoreVariants[1] = 't';
+                scoreVariants[3] = 't';
             }
         });
 
         gardensVariant.setOnAction(event -> {
             if (gardensVariant.isSelected()) {
-                scoreVariants[1] = 'G';
+                scoreVariants[4] = 'G';
             } else {
-                scoreVariants[1] = 'g';
+                scoreVariants[4] = 'g';
             }
         });
 
-        // Add grey box background for controls
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(greyBox,playerCountHBox);
-        controls.getChildren().add(stackPane);
-    }
-
-
-    public static void makeView() {
-        // Create background image
-        Image backgroundImage = new Image(
-                "https://cf.geekdo-images.com/OTb0hIJHE-U3eD0FmpBrzA__opengraph/img/gYkj6_qAS2W_vj-uNP92cWCbakc=/0x0:3060x1607/fit-in/1200x630/filters:strip_icc()/pic6705097.jpg",
-                SETUP_WIDTH, SETUP_HEIGHT, false, false);
-        BackgroundImage BI = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT,
-                BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-        Background BG = new Background(BI);
-        view.setBackground(BG);
-    }
-
-    public static String display() {
-        primaryStage.setTitle("Setup");
-        makeControls();
-        makeView();
-        root.getChildren().addAll(controls, view);
-
-        // Create the scene and display everything
-        Scene scene = new Scene(root, SETUP_WIDTH, SETUP_HEIGHT);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
+        /* If the playButton is hit the game will start, the play button generates
+           the first gameState string that will start the game                     */
         playButton.setOnAction(e -> {
             String settings = playerCount + new String(scoreVariants) + ";";
             // Initialize tileID for playerCount
             int maxTileID;
             switch (playerCount) {
-                case 2:
-                    maxTileID = 37;
-                    break;
-                case 3:
-                    maxTileID = 49;
-                    break;
-                default:
-                    maxTileID = 61;
-                    break;
+                case 2: maxTileID = 37; break;
+                case 3: maxTileID = 49; break;
+                default: maxTileID = 61; break;
             }
-            String shared = "0";
-            for (int i = 0; i < 5; i++) {
+
+            // Initialize shared to "0"
+            StringBuilder shared = new StringBuilder("0");
+
+            // Determine size of the construction site based on playerCount
+            int constructionSiteSize;
+            switch (playerCount) {
+                case 2: constructionSiteSize = 4; break;
+                case 3: constructionSiteSize = 5; break;
+                default: constructionSiteSize = 6; break;
+            }
+
+            // Create the construction site
+            java.util.Set<String> generatedTileIDs = new HashSet<>();
+            while (generatedTileIDs.size() < constructionSiteSize) {
                 Random random = new Random();
                 int tileID = random.nextInt(maxTileID + 1);
+                String tileIDString;
+
                 if (tileID < 10) {
-                    shared += "0" + tileID;
+                    tileIDString = "0" + tileID;
                 } else {
-                    shared += tileID;
+                    tileIDString = String.valueOf(tileID);
+                }
+
+                if (!generatedTileIDs.contains(tileIDString)) {
+                    generatedTileIDs.add(tileIDString);
                 }
             }
-            shared += ";";
 
+            // Append the construction site onto the shared string
+            for (String tileID : generatedTileIDs) {
+                shared.append(tileID);
+            }
+            shared.append(";");
             String playerStrings;
             switch (playerCount) {
                 case 2:
@@ -238,12 +218,41 @@ public class Setup {
             }
             gameState = settings + shared + playerStrings;
             primaryStage.close();
-        });
 
-        // Print and return gameState string when button is clicked
-        System.out.println(gameState);
-        return gameState;
+
+            // Invoke a callback method with the gameState to return the gameState to start the game
+            // todo Akropolis.onGameStart(gameState);
+
+            System.out.println(gameState);
+        });
     }
+
+    // Method to make background
+    public static void makeBackground() {
+        // Create background image
+        Image backgroundImage = new Image("https://cf.geekdo-images.com/OTb0hIJHE-U3eD0FmpBrzA__opengraph/img/gYkj6_qAS2W_vj-uNP92cWCbakc=/0x0:3060x1607/fit-in/1200x630/filters:strip_icc()/pic6705097.jpg",
+                SETUP_WIDTH, SETUP_HEIGHT, false, true);
+        BackgroundImage BI = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        Background BG = new Background(BI);
+        background.setBackground(BG);
+    }
+    public static void display() {
+        primaryStage.setTitle("Setup");
+        makeBackground();
+        makeControls();
+        controls.setTranslateY(100);
+
+        root.getChildren().addAll(background, controls);
+
+
+        Scene scene = new Scene(root, SETUP_WIDTH, SETUP_HEIGHT);
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 
 }
 
