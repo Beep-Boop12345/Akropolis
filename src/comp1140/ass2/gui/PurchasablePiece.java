87a8c7ba13fbbf8e4ed9e1b5c701c28c2931c030
Andrew Nguyen <u7330006@anu.edu.au>
2023-10-20 13:00:37 +1100

    Add sound but can't find the file

--- src/comp1140/ass2/gui/PurchasablePiece.java
+++ src/comp1140/ass2/gui/PurchasablePiece.java
@@ -154,6 +154,7 @@ public class PurchasablePiece extends Group {
                 updateShape();
             }
             akropolis.applyMove(moveSelected);
+            viewer.playSound();
             viewer.updateView();
         });
     }
